71934a2170eec64ecce4761c67213949576aea6b
jhin93 <scene1993@gmail.com>
2025-07-23 21:41:50 +1000

    code pipeline finish

--- infra/aws/lib/aws-stack.ts
+++ infra/aws/lib/aws-stack.ts
@@ -3,6 +3,8 @@ import { Construct } from 'constructs';
 import { Vpc } from 'aws-cdk-lib/aws-ec2';
 import { Cluster } from 'aws-cdk-lib/aws-ecs';
 import { Bucket } from 'aws-cdk-lib/aws-s3';
+import { Project } from 'aws-cdk-lib/aws-codebuild';
+import { Pipeline } from 'aws-cdk-lib/aws-codepipeline';
 
 export class AwsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -18,9 +20,18 @@ export class AwsStack extends cdk.Stack {
       clusterName: 'SP-01-ECS',
       vpc: vpc, // Use the existing VPC
     });
-
+    // Importing an existing S3 bucket
     const s3Bucket = Bucket.fromBucketAttributes(this, 'ImportedBucket', {
       bucketName: 'sp01-logjack',
     });
+    // CodePipeline - CodeBuild integration
+    const codeBuildProject = Project.fromProjectName(this, 'ImportedBuildProject', 'SP-01');
+    // CodePipline - CodePipeline integration
+    const codePipeline = Pipeline.fromPipelineArn(
+        this,
+        'ImportedPipeline',
+        'arn:aws:codepipeline:ap-southeast-2:221196769151:SP-01'
+    );
+    
   };
 };
